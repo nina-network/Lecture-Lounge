c1488569373729a19dfc33d0288e61fc6da3083c
nina-network <serenalee357@gmail.com>
2024-03-27 11:42:08 -0400

    add room route and html

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def login_page():
 @app.route('/profile')
 def profile_page():
     return render_template('profile.html')
+
+@app.get('/room')
+def room():
+	return render_template('room.html', room = room)
