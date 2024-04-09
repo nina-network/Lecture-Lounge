8b1a2ab19bd14e4df57d1c4935c83599154a63fc
Carson Bavery <ctrackb31@gmail.com>
2024-04-09 16:36:40 -0400

    Created the base HTML for the Login Page

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ def index():
 
 @app.route('/login')
 def login_page():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'your_username' and password == 'your_password':
+            return redirect(url_for('profile_page'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
+    # Render login page for GET request
     return render_template('login.html')
 
 @app.route('/profile')
