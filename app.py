8b8e5c60dd2172611abd2a903f012def3832298e
Shae Eggert <shae@MacBook-Pro-195.lan>
2024-04-16 17:06:54 -0400

    Adding pytests and post request for rooms

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, abort
+from random import randint
 
 app = Flask(__name__)
+
+rooms = {}
+room_texts = {}
 
 @app.route('/')
 def index():
@@ -20,7 +24,20 @@ def profile_page():
 
 @app.get('/room')
 def room():
-	return render_template('room.html')
+	return render_template('room.html', rooms=rooms, room_texts=room_texts)
+
+
+# name and text -- reconfigure after database is integrated
+@app.post('/room')
+def create_rooms():
+    room_name = request.form.get('room_name')
+    room_text = request.form.get('room_text')
+    if not room_name:
+        abort(400)
+    room_id = randint(1,1000)
+    rooms[room_id] = room_name
+    room_texts[room_id] = room_text
+    return redirect('/room')
 
 @app.get('/createroom')
 def create_room():
