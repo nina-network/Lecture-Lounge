648525ec6e0c8ff502cbf67437a675f86308c649
Chad <107705900+carrchadd@users.noreply.github.com>
2024-04-18 20:47:12 -0400

    Adding home page dynamic functionality and other changes

--- repositories/post_repository.py
+++ repositories/post_repository.py
@@ -1,2 +1,11 @@
 from repositories.db import get_pool
 from psycopg.rows import dict_row
+
+
+# get all posts by room
+def get_all_posts():
+    pool = get_pool()
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=dict_row) as cursor:
+            cursor.execute('SELECT * FROM posts;')
+            return cursor.fetchall()
